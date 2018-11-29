2a1ea2ba530fb1cfea50647da5c9d43b48a2c135
Maltego Staff <support@maltego.com>
2018-11-29 15:52:30 +0200

    Update NameFromCSV.py

--- transforms/NameFromCSV.py
+++ transforms/NameFromCSV.py
@@ -1,6 +1,6 @@
 from maltego_trx.entities import Person
-from maltego_trx.transform import DiscoverableTransform
 from maltego_trx.maltego import UIM_PARTIAL
+from maltego_trx.transform import DiscoverableTransform
 
 
 class NameFromCSV(DiscoverableTransform):
@@ -11,26 +11,27 @@ class NameFromCSV(DiscoverableTransform):
     @classmethod
     def create_entities(cls, request, response):
         phone = request.Value
-        
+
         try:
-            name = cls.get_name(phone)
-            if name:
-                response.addEntity(Person, name)
+            names = cls.get_names(phone)
+            if names:
+                for name in names:
+                    response.addEntity(Person, name)
             else:
                 response.addUIMessage("The phone number given did not match any numbers in the CSV file")
         except IOError:
             response.addUIMessage("An error occurred reading the CSV file.", messageType=UIM_PARTIAL)
-    
-    
+
     @staticmethod
-    def get_name(search_phone):
+    def get_names(search_phone):
+        matching_names = []
         with open("phone_to_names.csv") as f:
             for ln in f.readlines():
                 phone, name = ln.split(",", 1)
                 if phone.strip() == search_phone.strip():
-                    return name.strip()
-                    
+                    matching_names.append(name.strip())
+        return matching_names
 
-                    
+
 if __name__ == "__main__":
-    print(NameFromCSV.get_name("1-541-754-3010"))
+    print(NameFromCSV.get_names("1-541-754-3010"))
